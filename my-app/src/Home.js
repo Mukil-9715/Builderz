d7b30424ad99e03c8a19393be7874bfa4aec3fca
Mukil-9715 <mukilansuriya9715@gmail.com>
2023-06-21 23:50:47 +0530

    new learnings

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.css";
 import { Link } from "react-router-dom";
 import home from "./png/bghome1.png";
-
-
-
 import Nav from './Nav'
 import About from './About'
 import Project from './Project'
 import Service from './Service'
 import Footer from './Footer'
+import axios from 'axios'
+
+
 
 const Home = () => {
+  const [message, setMessage] = useState(''); 
+
+  useEffect(() => {
+    const getData = async () => {
+      let got = await axios.get("https://api-server.hostrain.in/api/homepage");
+      setMessage(got.data.message);
+    };
+
+    getData();
+  }, []);
+  console.log('message',message)
   return (
     <div className="master_container">
 
@@ -20,7 +31,8 @@ const Home = () => {
       <div className="section_1">
         <div className="left">
           <div className="texth1">
-            We construct <br></br> your <span className="outline">Dream</span>
+            {message}
+            {/* We construct <br></br> your <span className="outline">Dream</span> */}
           </div>
           <div className="tp">
             <div><Link className='a' to="/Project">Discover Project</Link></div>
@@ -33,7 +45,7 @@ const Home = () => {
       </div>
 
 
-      <About />
+      {message && <About mukil={message}/>}
       <Project />
       <Service />
       <Footer />
